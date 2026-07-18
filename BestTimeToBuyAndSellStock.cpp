4a4a3fcf597ff503f7355fd323bdbb3e94c7027a
Robinhood75t <robinparjapatikumar@gmail.com>
2026-07-18 10:42:06 +0530

    BestTimeToBuyAndSellStock

---
+++ BestTimeToBuyAndSellStock.cpp
@@ -0,0 +1,19 @@
+// Brute Force Solution
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int maxProfit = -1;
+        for(int i = 0; i < n; i++){
+            int buy = prices[i];
+            for(int j = i + 1; j < n; j++){
+                int sell = prices[j];
+                maxProfit = max(sell - buy, maxProfit);
+            }
+        }
+        return maxProfit == -1 ? 0 : maxProfit;
+    }
+};
+// Time Complexity: O(n^2)
+// Space Complexity: O(1)
+
