198f4a2efba2ac9e080748acd50911ece5d20806
Robinhood75t <robinparjapatikumar@gmail.com>
2026-07-18 10:44:44 +0530

    BTTBASS

--- BestTimeToBuyAndSellStock.cpp
+++ BestTimeToBuyAndSellStock.cpp
@@ -17,3 +17,21 @@ public:
 // Time Complexity: O(n^2)
 // Space Complexity: O(1)
 
+// Optimized Solution
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int maxProfit = -1;
+        int minSoFar = INT_MAX;
+        for(int i = 0; i < n; i++){
+            minSoFar = min(prices[i],minSoFar);
+            int profit = prices[i] - minSoFar;
+            maxProfit = max(profit,maxProfit);
+        }
+        return maxProfit == -1 ? 0 : maxProfit;
+    }
+};
+// Time Complexity: O(n)
+// Space Complexity: O(1) because we are using only a few variables 
+// this approach can be called Gready approach 
